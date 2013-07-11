1b706011fe9e8a23bdc4096edee984de645de919
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2013-07-11 14:51:23 -0400

    Fix broken import

--- mandoline/mandoline.py
+++ mandoline/mandoline.py
@@ -5,7 +5,7 @@ import json
 
 from requests import get as http_get, put as http_put
 
-boto.s3.connection import S3Connection
+from boto.s3.connection import S3Connection
 import os.environ
 
 from cleaners import *
